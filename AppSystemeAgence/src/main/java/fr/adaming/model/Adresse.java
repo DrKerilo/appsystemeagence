207e7e6d4a473304af73dc070608131d5f59e0a4
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-04-30 15:25:11 +0200

    Val: commit adresse embeddable

---
+++ AppSystemeAgence/src/main/java/fr/adaming/model/Adresse.java
@@ -0,0 +1,59 @@
+package fr.adaming.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable	// Classe embarquée
+public class Adresse {
+
+	// Attributs
+	private String rue;
+	private int numero;
+	private int codePostal;
+	private String localite;
+	
+	// Constructeurs
+	public Adresse() {
+		super();
+	}
+	public Adresse(String rue, int numero, int codePostal, String localite) {
+		super();
+		this.rue = rue;
+		this.numero = numero;
+		this.codePostal = codePostal;
+		this.localite = localite;
+	}
+	
+	// Getters et setters
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getLocalite() {
+		return localite;
+	}
+	public void setLocalite(String localite) {
+		this.localite = localite;
+	}
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Adresse [rue=" + rue + ", numero=" + numero + ", codePostal=" + codePostal + ", localite=" + localite
+				+ "]";
+	}
+	
+}
