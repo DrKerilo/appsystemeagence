3af49c62f8fdd6a0b25a882f6cd1664e3085aa13
inti0340 <inti0340@DESKTOP-9KI76JQ>
2018-04-30 16:11:59 +0200

    Camille: ClasseStandardRest

---
+++ AppSystemeAgence/src/main/java/ClasseStandardRest.java
@@ -0,0 +1,4 @@
+
+public class ClasseStandardRest {
+
+}
---
+++ AppSystemeAgence/src/main/java/fr/adaming/rest/ClasseStandardRest.java
@@ -0,0 +1,46 @@
+package fr.adaming.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.ClasseStandard;
+import fr.adaming.service.IClasseStandardService;
+
+@RestController
+public class ClasseStandardRest {
+	
+	@Autowired
+	private IClasseStandardService csService;
+		
+	@RequestMapping(value="/classeStandard", method=RequestMethod.POST, consumes="application/json", produces="application/json")
+	public ClasseStandard addClasseStandard(ClasseStandard cs) {
+		return csService.addClasseStandard(cs);
+	}
+	
+	@RequestMapping(value="/classeStandard", method=RequestMethod.PUT, consumes="application/json", produces="application/json")
+	public ClasseStandard updateClasseStandard(ClasseStandard cs) {
+		return csService.updateClasseStandard(cs);
+	}
+	
+	@RequestMapping(value="/classeStandard/{pId}", method=RequestMethod.DELETE)
+	public int deleteClasseStandard(@PathVariable("pId") int id) {
+		return csService.deleteClasseStandard(id);
+	}
+	
+	@RequestMapping(value="/listeCS", method=RequestMethod.GET, produces="application/json")
+	public List<ClasseStandard> getAllClasseStandard() {
+		return csService.getAllClasseStandard();
+	}
+	
+	@RequestMapping(value="/classeStandard", method=RequestMethod.GET, produces="application/json")
+	public ClasseStandard getClasseStandardById(@RequestParam("pId")int id) {
+		return csService.getClasseStandardById(id);
+	}
+
+}
