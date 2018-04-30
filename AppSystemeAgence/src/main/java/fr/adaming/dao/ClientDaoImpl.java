3e3d98f70da6a47e352f7e0423bc00b35fa601fc
inti0426 <inti0426@DESKTOP-5QL27UL>
2018-04-30 16:17:55 +0200

    Commit IClientDao, ClientDaoImpl

---
+++ AppSystemeAgence/src/main/java/fr/adaming/dao/ClientDaoImpl.java
@@ -0,0 +1,36 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import fr.adaming.model.Client;
+
+@Repository
+public class ClientDaoImpl implements IClientDao{
+
+	@Override
+	public Client addClient(Client cl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> getAllClient(Client cl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client updateClient(Client cl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteClient() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ AppSystemeAgence/src/main/java/fr/adaming/dao/IClientDao.java
@@ -0,0 +1,17 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.model.Client;
+
+public interface IClientDao {
+	
+	public Client addClient(Client cl);
+	
+	public List<Client> getAllClient(Client cl);
+	
+	public Client updateClient(Client cl);
+	
+	public int deleteClient();
+
+}
