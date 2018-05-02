46b949b74d68450c96f789315ee14ccf60d6799f
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-05-02 15:55:37 +0200

    Fab  : Angular bien afficher

---
+++ AppSystemeAgence/src/main/webapp/assets/js/controllers/bienImmobController.js
@@ -0,0 +1,8 @@
+// Controllers pour ClasseStandard
+app.controller("listeBICtrl", function($rootScope,$scope,$location,biService) {
+	
+	// Fonction appelée dès l'affichage de la vue
+	biService.getAllBi(function(callback) {
+		$scope.listeBiensImmobilier=callback;
+	})
+})
---
+++ AppSystemeAgence/src/main/webapp/assets/js/services/BienImmobProvider.js
@@ -0,0 +1,26 @@
+// ===== ClasseStandard =====
+// Services pour gérer les données de BienImmobilier
+app.factory("biService", function($http,$window) {
+	
+	// Variable globale du service
+	var restUrl="http://localhost:8080/AppSystemeAgence";
+	
+	// Déclaration externe des fonctions
+	// ----- Afficher liste
+	function findAllBi(busSC){
+		$http({
+			method:"GET",
+			url:restUrl+"/listBI"
+		}).then(function successCallback(response) {
+			console.log(response.data);
+			busSC(response.data);
+		}, function errorCallback(reason) {
+			console.log("Erreur "+reason.status+": "+reason.statusText);
+		})
+	};
+	
+	return {
+		getAllBi:findAllBi
+	}
+	
+})
