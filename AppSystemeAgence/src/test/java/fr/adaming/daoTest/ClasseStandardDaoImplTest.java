5fc0ab249a2486888eb2e3c1526769e45727475b
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-05-02 09:48:12 +0200

    [Val] Tests ClasseStandardDao et Service OK (mtnt ignorés)

--- AppSystemeAgence/src/test/java/fr/adaming/daoTest/ClasseStandardDaoImplTest.java
+++ AppSystemeAgence/src/test/java/fr/adaming/daoTest/ClasseStandardDaoImplTest.java
@@ -15,73 +15,76 @@ import fr.adaming.dao.IClasseStandardDao;
 import fr.adaming.model.ClasseStandard;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/applicationContext.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
 public class ClasseStandardDaoImplTest {
 
 	// Classe à tester
 	@Autowired
 	private IClasseStandardDao csDao;
-	
+
 	// ----- Test READ ALL
 	// Test 1 : taille de la liste
-//	@Ignore
-	@Test	
-	@Transactional(readOnly=true)
-	public void test1GetAllClasseStandardDao(){
+	@Ignore
+	@Test
+	@Transactional(readOnly = true)
+	public void test1GetAllClasseStandardDao() {
 		double tailleAtt = 2;
 		double tailleReelle = csDao.getAllClasseStandard().size();
-		
+
 		assertEquals(new Double(tailleAtt), new Double(tailleReelle));
-		
+
 	}
+
 	// Test 2 : liste non vide - test sur type
-//	@Ignore
-	@Test	
-	@Transactional(readOnly=true)
-	public void test2GetAllClasseStandardDao(){
+	@Ignore
+	@Test
+	@Transactional(readOnly = true)
+	public void test2GetAllClasseStandardDao() {
 		assertEquals("maison", csDao.getAllClasseStandard().get(0).getType());
 	}
-	
+
 	// ----- Test READ ONE
-//	@Ignore
+	@Ignore
 	@Test
-	@Transactional(readOnly=true)
-	public void testGetClasseStandardByIdDao(){
-		ClasseStandard csIn = csDao.getClasseStandardById(1);		// Vérifier valeur de l'id
-		
-		assertEquals("maison", csIn.getType());	// Vérifier type correspondant à l'id donné
-		
+	@Transactional(readOnly = true)
+	public void testGetClasseStandardByIdDao() {
+		ClasseStandard csIn = csDao.getClasseStandardById(1); // Vérifier valeur
+																// de l'id
+
+		assertEquals("maison", csIn.getType()); // Vérifier type correspondant à
+												// l'id donné
+
 	}
-	
+
 	// ----- Test CREATE
-//	@Ignore
+	@Ignore
 	@Test
 	@Transactional
 	@Rollback(true)
-	public void testAddClasseStandardDao(){
+	public void testAddClasseStandardDao() {
 		// Instanciation objet vide
 		ClasseStandard csIn = new ClasseStandard();
-		
-		int tailleAvant=csDao.getAllClasseStandard().size();
-		
+
+		int tailleAvant = csDao.getAllClasseStandard().size();
+
 		csDao.addClasseStandard(csIn);
-		
+
 		assertEquals(new Double(++tailleAvant), new Double(csDao.getAllClasseStandard().size()));
 	}
-	
+
 	// ----- Test UPDATE
-//	@Ignore
+	@Ignore
 	@Test
 	@Transactional
 	@Rollback(true)
-	public void testUpdateClasseStandardDao(){
+	public void testUpdateClasseStandardDao() {
 		// Récupération d'un objet de la BD
 		ClasseStandard csModif = csDao.getClasseStandardById(1);
-		
+
 		csModif.setType("TEST");
-		
+
 		assertEquals("TEST", csDao.updateClasseStandard(csModif).getType());
-		
+
 	}
-	
+
 }
--- AppSystemeAgence/src/test/java/fr/adaming/serviceTest/ClasseStandardServiceImplTest.java
+++ AppSystemeAgence/src/test/java/fr/adaming/serviceTest/ClasseStandardServiceImplTest.java
@@ -1,5 +1,89 @@
 package fr.adaming.serviceTest;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.model.ClasseStandard;
+import fr.adaming.service.IClasseStandardService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
 public class ClasseStandardServiceImplTest {
 
+	// Classe à tester
+	@Autowired
+	private IClasseStandardService csService;
+
+	// ----- Test READ ALL
+	// Test 1 : taille de la liste
+	@Ignore
+	@Test
+	@Transactional(readOnly = true)
+	public void test1GetAllClasseStandardService() {
+		double tailleAtt = 2;
+		double tailleReelle = csService.getAllClasseStandard().size();
+
+		assertEquals(new Double(tailleAtt), new Double(tailleReelle));
+	}
+
+	// Test 2 : liste non vide - test sur type
+	@Ignore
+	@Test
+	@Transactional(readOnly = true)
+	public void test2GetAllClasseStandardService() {
+		assertEquals("maison", csService.getAllClasseStandard().get(0).getType());
+	}
+
+	// ----- Test READ ONE
+	@Ignore
+	@Test
+	@Transactional(readOnly = true)
+	public void testGetClasseStandardByIdService() {
+		ClasseStandard csIn = csService.getClasseStandardById(1); // Vérifier
+																	// valeur
+																	// de l'id
+
+		assertEquals("maison", csIn.getType()); // Vérifier type correspondant à
+												// l'id donné
+
+	}
+
+	// ----- Test CREATE
+	@Ignore
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testAddClasseStandardService() {
+		// Instanciation objet vide
+		ClasseStandard csIn = new ClasseStandard();
+
+		int tailleAvant = csService.getAllClasseStandard().size();
+
+		csService.addClasseStandard(csIn);
+
+		assertEquals(new Double(++tailleAvant), new Double(csService.getAllClasseStandard().size()));
+	}
+
+	// ----- Test UPDATE
+	@Ignore
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testUpdateClasseStandardService() {
+		// Récupération d'un objet de la BD
+		ClasseStandard csModif = csService.getClasseStandardById(1);
+
+		csModif.setType("TEST");
+
+		assertEquals("TEST", csService.updateClasseStandard(csModif).getType());
+
+	}
 }
