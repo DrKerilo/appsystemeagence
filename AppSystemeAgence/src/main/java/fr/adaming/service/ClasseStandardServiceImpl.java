735475b77ae0d5baaff244922dbafcb202418ea0
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-05-02 09:52:30 +0200

    [Val] Ajout @Transactionnal CSServiceImpl

--- AppSystemeAgence/src/main/java/fr/adaming/service/ClasseStandardServiceImpl.java
+++ AppSystemeAgence/src/main/java/fr/adaming/service/ClasseStandardServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.dao.IClasseStandardDao;
 import fr.adaming.model.ClasseStandard;
 
 @Service
+@Transactional
 public class ClasseStandardServiceImpl implements IClasseStandardService {
 	
 	// Injection de dépendance
