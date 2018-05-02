c220d232a18ecf63d475cab8e1d7e5fb596e4600
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-05-02 18:14:30 +0200

    [Val] CRUD Front CS

--- AppSystemeAgence/src/main/webapp/assets/js/controllers/classeStandardController.js
+++ AppSystemeAgence/src/main/webapp/assets/js/controllers/classeStandardController.js
@@ -1,8 +1,131 @@
 // Controllers pour ClasseStandard
-app.controller("listeCSCtrl", function($rootScope,$scope,$location,csService) {
-	
-	// Fonction appelée dès l'affichage de la vue
-	csService.getAll(function(callback) {
-		$scope.listeClassesStandards=callback;
-	})
+app.controller("listeCSCtrl",
+		function($rootScope, $scope, $location, csService) {
+
+			// Fonction appelée dès l'affichage de la vue
+			csService.getAll(function(callback) {
+				$scope.listeClassesStandards = callback;
+			});
+
+			// Déclaration de csModif avec un id undefined
+			$rootScope.csModif = {
+				code : undefined
+			};
+
+			// Fonction appelée via le lien Modifier
+			$scope.modifierLien = function(classeStand) {
+				$rootScope.csModif = classeStand;
+				$location.path("updateCS");
+			};
+
+			// Fonction appelée via le lien Supprimer
+			$scope.supprimerLien = function(idClasse) {
+				csService.deleteOne(idClasse, function(callback) {
+					if (callback == "OK") {
+						csService.getAll(function(callback) {
+							$scope.listeClassesStandards = callback;
+						});
+					} else {
+						$scope.message="Erreur: la classe n'a pas pu être supprimée."
+					}
+				});
+			};
+
+		})
+
+.controller("ajoutCSCtrl", function($scope, $location, csService) {
+
+	// Variables
+	$scope.cs = { // Déclaration d'une classe standard
+		modeOffre : "",
+		prixMax : "",
+		superficieMin : "",
+		type : ""
+	};
+
+	// Fonction appelée via le bouton Ajouter
+	$scope.ajouterCS = function() {
+		csService.addOne($scope.cs, function(callback) {
+			if (callback == "OK") {
+				$location.path("listCS");
+			} else {
+				$scope.message = "Erreur: l'ajout de la classe n'a pas abouti."
+			}
+		})
+	};
 })
+
+.controller("modifCSCtrl", function($rootScope, $scope, $location, csService) {
+
+	// Variables
+	$scope.indice = false;
+
+	if ($rootScope.csModif.code == undefined) {
+		$scope.cs = { // Déclaration d'une classe standard
+			code : "",
+			modeOffre : "",
+			prixMax : "",
+			superficieMin : "",
+			type : ""
+		};
+	} else {
+		$scope.cs = $rootScope.csModif;
+	}
+
+	// Fonction appelée via le bouton Modifier
+	$scope.modifierCS = function() {
+		csService.updateOne($scope.cs, function(callback) {
+			if (callback == "OK") {
+				$location.path("listCS");
+				$scope.indice = false;
+			} else {
+				$scope.indice = true;
+				$scope.message = "Erreur: cette classe standard n'existe pas.";
+			}
+		});
+	}
+})
+
+.controller("supprCSCtrl", function($scope,$location,csService) {
+	
+	// Variables
+	$scope.id;
+	$scope.indice=false;
+	
+	// Fonction appelée via le bouton Supprimer
+	$scope.supprimerCS=function(){
+		csService.deleteOne($scope.id,function(callback){
+			if (callback == "OK") {
+				$location.path("listCS");
+				$scope.indice = false;
+			} else {
+				$scope.indice = true;
+				$scope.message = "Erreur: cette classe standard n'existe pas.";
+			}
+		});
+	}
+})
+
+.controller("rechCSCtrl", function($scope,csService) {
+	
+	// Variables
+	$scope.id;
+	$scope.indiceOK=false;
+	$scope.indiceNOT=false;
+	
+	// Fonction appelée via le bouton Chercher
+	$scope.rechercherCS=function(){
+		csService.getOne($scope.id,function(callback){
+			if(typeof callback == "object"){
+				$scope.csOut=callback;
+				$scope.indiceOK=true;
+				$scope.indiceNOT=false;
+			} else {
+				$scope.indiceOK=false;
+				$scope.indiceNOT=true;
+				$scope.message="Erreur: cette classe standard n'existe pas.";
+			}
+		})
+	}
+
+});
--- AppSystemeAgence/src/main/webapp/assets/js/services/ClasseStandardProvider.js
+++ AppSystemeAgence/src/main/webapp/assets/js/services/ClasseStandardProvider.js
@@ -1,25 +1,86 @@
 // ===== ClasseStandard =====
 // Services pour gérer les données de ClasseStandard
-app.factory("csService", function($http,$window) {
-	
+app.factory("csService", function($http, $window) {
+
 	// Variable globale du service
-	var restUrl="http://localhost:8080/AppSystemeAgence/";
-	
+	var restUrl = "http://localhost:8080/AppSystemeAgence/";
+
 	// Déclaration externe des fonctions
 	// ----- Afficher liste
-	function findAll(busSC){
+	function findAll(busSC) {
 		$http({
-			method:"GET",
-			url:restUrl+"/listeCS"
+			method : "GET",
+			url : restUrl + "/listeCS"
 		}).then(function successCallback(response) {
 			busSC(response.data);
 		}, function errorCallback(reason) {
-			console.log("Erreur "+reason.status+": "+reason.statusText);
+			console.log("Erreur " + reason.status + ": " + reason.statusText);
 		})
-	};
-	
-	return {
-		getAll:findAll
+	}
+	;
+
+	// ----- Ajouter
+	function addClasse(cs, busSC) {
+		$http({
+			method : "POST",
+			url : restUrl + "/classeStandard",
+			data : JSON.stringify(cs),
+			headers : {
+				"Content-Type" : "application/json"
+			}
+		}).then(function successCallback(response) {
+			busSC(response.statusText);
+		}, function errorCallback(reason) {
+			console.log("Erreur " + reason.status + ": " + reason.statusText);
+		})
+	}
+	;
+
+	// ----- Modifier
+	function updateClasse(cs, busSC) {
+		$http({
+			method : "PUT",
+			url : restUrl + "/classeStandard",
+			data : JSON.stringify(cs),
+			headers : {
+				"Content-Type" : "application/json"
+			}
+		}).then(function successCallback(response) {
+			busSC(response.status)
+		}, function errorCallback(reason) {
+			busSC(reason.status);
+			console.log("Erreur " + reason.status + ": " + reason.statusText);
+		})
 	}
 	
+	// ----- Supprimer
+	function deleteClasse(id,busSC){
+		if($window.confirm("Êtes-vous sûr de vouloir supprimer cette classe ?")){
+			$http.delete(restUrl+"/classeStandard/"+id)
+			.then(function successCallback(response) {
+				busSC(response.statusText);
+			}, function errorCallback(reason) {
+				console.log("Erreur "+reason.status+": "+reason.statusText);
+			});
+		}
+	};
+	
+	// ----- Chercher par id
+	function findClasse(id,busSC){
+		$http.get(restUrl+"/classeStandard?pId="+id)
+				.then(function successCallback(response) {
+					busSC(response.data);
+				}, function errorCallback(reason) {
+					console.log("Erreur "+reason.status+": "+reason.statusText);
+				})
+	};
+
+	return {
+		getAll : findAll,
+		addOne : addClasse,
+		updateOne : updateClasse,
+		deleteOne : deleteClasse,
+		getOne : findClasse
+	}
+
 })
