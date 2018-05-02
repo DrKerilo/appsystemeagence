541c9e1d7687bb01eee7db460d2ad3ebf55e83a8
inti0340 <inti0340@DESKTOP-9KI76JQ>
2018-05-02 16:37:02 +0200

    Camille: modif modele Agent

--- AppSystemeAgence/src/main/java/fr/adaming/model/Agent.java
+++ AppSystemeAgence/src/main/java/fr/adaming/model/Agent.java
@@ -25,6 +25,7 @@ public class Agent implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id_a")
 	private int id;
+	private String identifiant;
 	private String mdp;
 	
 	@JsonIgnore
@@ -34,16 +35,19 @@ public class Agent implements Serializable{
 	// déclaration des constructeurs
 	public Agent() {
 		super();
-	}
-	
-	public Agent(String mdp) {
+	}	
+
+	public Agent(String identifiant, String mdp) {
 		super();
+		this.identifiant = identifiant;
 		this.mdp = mdp;
 	}
+	
 
-	public Agent(int id, String mdp) {
+	public Agent(int id, String identifiant, String mdp) {
 		super();
 		this.id = id;
+		this.identifiant = identifiant;
 		this.mdp = mdp;
 	}
 
@@ -54,6 +58,14 @@ public class Agent implements Serializable{
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public String getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
 	}
 
 	public String getMdp() {
